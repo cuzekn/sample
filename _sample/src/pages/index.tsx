8a107a1c0306e7a9afc18bcb52e0ea2b4e85751f
cuze <cuzekn@gmail.com>
2022-05-02 01:11:52 +0900

    feat: checkboxの切り替え機能を実装

--- _sample/src/pages/index.tsx
+++ _sample/src/pages/index.tsx
@@ -1,20 +1,41 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
-const TODOS = [
+type Todo = {
+  id: number;
+  text: string;
+  isDone: boolean;
+};
+
+const TODOS: Todo[] = [
   { id: 1, text: "foo", isDone: false },
   { id: 2, text: "bar", isDone: true },
 ];
 
 const Home: NextPage = () => {
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
+
+  const toggleIsDone = (id: Todo["id"]) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+    });
+  };
+
   return (
     <div>
       <h3>TODO一覧</h3>
-      {TODOS.map((todo) => (
+      {todos.map((todo) => (
         <div key={todo.id}>
           <label style={{ fontSize: "2rem" }}>
             <input
               type="checkbox"
               checked={todo.isDone}
+              onChange={() => toggleIsDone(todo.id)}
               style={{ width: "1.5rem", height: "1.5rem" }}
             />
             {todo.text}
